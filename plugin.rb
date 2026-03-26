b5a5077bed157ac30c5f9d7fa9128299338d367b
ban2zai <aleskerov.02@gmail.com>
2026-03-26 16:25:05 +0700

    fix (rss)

--- plugin.rb
+++ plugin.rb
@@ -306,6 +306,58 @@ after_initialize do
     end
   end
 
+  # --- TopicView#recent_posts: anonymize dc:creator in RSS feeds ---
+  # The RSS template (show.rss.erb) calls rss_creator(post.user) directly,
+  # bypassing all serializer patches. We wrap anonymous posts in a decorator
+  # that returns the anonymous user instead, using a single batch DB query.
+
+  class ::AnonymousRssPostDecorator < SimpleDelegator
+    def initialize(post, anon_user)
+      super(post)
+      @anon_user = anon_user
+    end
+
+    def user
+      @anon_user
+    end
+  end
+
+  module ::AnonymousTopicViewRssExtension
+    def recent_posts
+      posts = super
+      return posts unless SiteSetting.anonymous_post_enabled
+
+      posts_array = posts.to_a
+      return posts_array if posts_array.empty?
+
+      post_ids = posts_array.map(&:id)
+      anon_post_ids =
+        PostCustomField
+          .where(post_id: post_ids, name: "is_anonymous_post", value: "1")
+          .pluck(:post_id)
+          .to_set
+
+      is_anon_topic = AnonymousPostHelper.anon_topic?(topic)
+      topic_owner_id = topic.user_id
+
+      return posts_array unless anon_post_ids.any? || is_anon_topic
+
+      anon_user =
+        AnonymousPostHelper.anonymous_user ||
+          OpenStruct.new(display_name: AnonymousPostHelper.anon_username)
+
+      posts_array.map do |post|
+        if anon_post_ids.include?(post.id) || (is_anon_topic && post.user_id == topic_owner_id)
+          AnonymousRssPostDecorator.new(post, anon_user)
+        else
+          post
+        end
+      end
+    end
+  end
+
+  TopicView.prepend(AnonymousTopicViewRssExtension)
+
   # --- TopicView#page_title: anonymize username in browser/crawler <title> tag ---
   # When navigating to a specific post (/t/slug/id/N), Discourse appends
   # "- #N by username" to the page title. This leaks the real author for
